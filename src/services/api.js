49d082600db2b87202c9877ffa021d51018604a6
cirotorres <ciro_3@hotmail.com>
2025-06-21 01:20:26 -0300

    ajustes .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    // baseURL: "http://localhost:3000",
-    baseURL: process.env.REACT_APP_API_URL
+    // baseURL: "http://localhost:3000", // Development
+     baseURL: process.env.REACT_APP_API_URL,
+    // baseURL: "https://item-list-7c2g.onrender.com", // Production
 });
 
 export default api;
